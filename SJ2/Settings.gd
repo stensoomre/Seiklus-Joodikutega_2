0fca59ec32f944ac314972aae53b6f7408ee2cdd
stensoomre <98737965+stensoomre@users.noreply.github.com>
2022-04-28 11:43:37 +0300

    Add files via upload

--- SJ2/Settings.gd
+++ SJ2/Settings.gd
@@ -1,7 +1,13 @@
 extends Area2D
 
-func _input_event(viewport, event, shape_idx):
-	if event.type == InputEvent.MOUSE_BUTTON \
-	and event.button_index == BUTTON_LEFT \
-	and event.pressed:
-		print("Clicked")
+func _unhandled_input(event):
+	if event is InputEventMouseButton:
+		if event.button_index == BUTTON_LEFT:
+			if event.pressed:
+				print("Left button was clicked at ", event.position)
+			else:
+				print("Left button was released")
+
+
+func _on_Area2D_input_event(viewport, event, shape_idx):
+	pass # Replace with function body.
